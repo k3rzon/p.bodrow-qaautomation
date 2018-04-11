65d198d710ee912e21c5eb03a35aabe059634072
k3rzon <k3rzon@bk.ru>
2018-04-11 20:57:50 +0300

    Lecture 2 Task 1 Ready

---
+++ lecture 2/task 1/src/test/java/MyTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://software-testing.ru");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
