0f938f3fa09a28ad0de8b2ecb3425c973d70a623
k3rzon <k3rzon@bk.ru>
2018-04-15 21:12:27 +0300

    ready

--- lecture 3/task 4/src/test/java/myTest.java
+++ lecture 3/task 4/src/test/java/myTest.java
@@ -1,41 +1,22 @@
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class myTest {
 
     private WebDriver driverc;
     private WebDriver driverf;
     private WebDriver driverie;
-    private WebDriverWait wait;
-
-    @Before
-    public void start() {
-        driverc = new ChromeDriver();
-        wait = new WebDriverWait(driverc,20);
-        driverf = new FirefoxDriver();
-        wait = new WebDriverWait(driverie,20);
-        driverie = new InternetExplorerDriver();
-        wait = new WebDriverWait(driverie,20);
-    }
 
     @Test
     public void test() {
-      //  driver.get("http://localhost/litecart/admin/login.php");
-        //driver.findElement(By.name("username")).sendKeys("admin");
-        //driver.findElement(By.name("password")).sendKeys("admin");
-        //driver.findElement(By.name("login")).click();
-        //wait.until(titleIs("My Store"));
-   }
+        driverc = new ChromeDriver();
+        driverf = new FirefoxDriver();
+        driverie = new InternetExplorerDriver();
+    }
 
     @After
     public void stop() {
