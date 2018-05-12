443eb7ed95bab31d2a5f97e96cca29c8f897b0d5
mihalichzh <38403717+mihalichzh@users.noreply.github.com>
2018-05-12 16:00:36 +0300

    Revert "Merge branch 'master' of https://github.com/k3rzon/p.bodrow-qaautomation"
    This reverts commit 323e7a67da9e4b52b23fd3f3ee1360e87820359f, reversing
    changes made to ff7e80b33bed5488feef16af02cc58ec956390f8.

--- lecture 3/task 4/src/test/java/myTest.java
+++ lecture 3/task 4/src/test/java/myTest.java
@@ -1,22 +1,42 @@
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class myTest {
 
     private WebDriver driverc;
     private WebDriver driverf;
     private WebDriver driverie;
+    private WebDriverWait wait;
 
-    @Test
-    public void test() {
+    @Before
+    public void start() {
         driverc = new ChromeDriver();
         driverf = new FirefoxDriver();
         driverie = new InternetExplorerDriver();
+
+        wait = new WebDriverWait(driverc,20);
+        wait = new WebDriverWait(driverf,20);
+        wait = new WebDriverWait(driverie,20);
     }
+
+    @Test
+    public void test() {
+      //  driver.get("http://localhost/litecart/admin/login.php");
+        //driver.findElement(By.name("username")).sendKeys("admin");
+        //driver.findElement(By.name("password")).sendKeys("admin");
+        //driver.findElement(By.name("login")).click();
+        //wait.until(titleIs("My Store"));
+   }
 
     @After
     public void stop() {
